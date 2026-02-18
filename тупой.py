be7ddf2d39b064013062a86e8a6da10808804c4c
DeeGree <deegree@gmail.com>
2026-02-18 15:21:29 +0300

    мой первый проект на Python

---
+++ тупой.py
@@ -0,0 +1,13 @@
+name = input("Привет! Как тебя зовут? ")
+while name == "":
+    name = input("Ещё паз: Как тебя зовут? ")
+city = input("В каком городе ты живёшь? ")
+while city == "":
+    city = input("Ещё раз: В каком городе ты живёшь? ")
+number = input("Какое твоё любимое число? ")
+while number == "" or number.isdigit() == False:   
+    number = input("Введи, пожалуйста, положительное число ")
+number = int(number)
+print(f"Привет, {name}, из города {city}!")
+print(f"Твоё любимое число - {number}")
+print(f"А если его умножить на 2, будет {number*2}")
