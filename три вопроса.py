6e22a108c1741da4360a56ee203660a0536bcaec
DeeGree <deegree@gmail.com>
2026-02-20 14:34:32 +0300

    три вопроса

--- три вопроса.py
+++ три вопроса.py
@@ -7,8 +7,14 @@ while name == "":
 age = input("Сколько тебе лет? ")
 while age.isdigit() == False:
     age = input("Ты ввёл текст, Введи цифровое значение! ")
+age = int(age)
+if age > 120:
+    print("Ты дебил")
+    else 
 color = input("Какой твой любимый цвет? ")
+while color == "":
+    color = input("Ты не указал цвет! ")
 print(f"Спасибо! Вот что ты рассказал:")    
 print(f"Тебя зовут {name}.")
-print(f"Тебе {age} лет.")
+print(f"Тебе {age} лет. Через 10 лет тебе будет {age + 10} лет")
 print(f"Твой любимый цвет {color}.")
