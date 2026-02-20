497b49b68eecafd4e79adf163c1b3c0ae9cd16f6
DeeGree <deegree@gmail.com>
2026-02-20 15:01:30 +0300

    три вопроса

--- три вопроса.py
+++ три вопроса.py
@@ -8,9 +8,9 @@ age = input("Сколько тебе лет? ")
 while age.isdigit() == False:
     age = input("Ты ввёл текст, Введи цифровое значение! ")
 age = int(age)
-if age > 120:
-    print("Ты дебил")
-    else 
+while age >= 120:
+    age = input("Ты дебил? Введи нормальный возраст! ")
+    age = int(age)
 color = input("Какой твой любимый цвет? ")
 while color == "":
     color = input("Ты не указал цвет! ")
